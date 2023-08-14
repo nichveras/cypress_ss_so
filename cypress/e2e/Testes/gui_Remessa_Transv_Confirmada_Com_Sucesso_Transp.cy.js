e724c86c31ccf0aafeb14568b66b2a84ac8ab450
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 16:03:52 -0300

    Update gui_Remessa_Transv_Confirmada_Com_Sucesso_Transp.cy.js

--- cypress/e2e/Testes/gui_Remessa_Transv_Confirmada_Com_Sucesso_Transp.cy.js
+++ cypress/e2e/Testes/gui_Remessa_Transv_Confirmada_Com_Sucesso_Transp.cy.js
@@ -5,7 +5,7 @@ describe('Teste funcional de Login Transportadora', () => {
     it('Deve realizar um login com sucesso', () => {
         cy.visit("https://souout-homolog.souenergy.com.br/")
         cy.get('#input-10').type("iulyaermolaewa@email-temp.com")
-        cy.get('#input-13').type("***8*")
+        cy.get('#input-13').type("*****")
         cy.get('.m-texto').click()   
         //Validação de Login
         cy.get('.v-toolbar__title').should('contain','Remessas da Transportadora')
