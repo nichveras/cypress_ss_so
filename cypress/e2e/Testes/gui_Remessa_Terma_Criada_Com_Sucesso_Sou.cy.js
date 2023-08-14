11e3be2166a6d78c28ae6b740455742905a78825
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 15:59:21 -0300

    Update gui_Remessa_Terma_Criada_Com_Sucesso_Sou.cy.js

--- cypress/e2e/Testes/gui_Remessa_Terma_Criada_Com_Sucesso_Sou.cy.js
+++ cypress/e2e/Testes/gui_Remessa_Terma_Criada_Com_Sucesso_Sou.cy.js
@@ -5,7 +5,7 @@ describe('Teste E2E - Realizando a criação de remessa com sucesso', () => {
     it('Fluxo da criação de remessa', () => {
             cy.visit("https://souout-homolog.souenergy.com.br/")
             cy.get('#input-10').type("Nicholas")
-            cy.get('#input-13').type("123456")
+            cy.get('#input-13').type("*****")
             cy.get('.m-texto').click()   
             //Validação de Login 
             cy.get('.v-toolbar__title').should('contain','Dashboard')                                                       
