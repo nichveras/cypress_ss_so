0e7fc2570aabde40a59e7ceab69ca73664611331
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 15:58:46 -0300

    Update gui_Login_ss.cy.js

--- cypress/e2e/Testes/gui_Login_ss.cy.js
+++ cypress/e2e/Testes/gui_Login_ss.cy.js
@@ -5,7 +5,7 @@ describe('Teste funcional de Login Sou Serials', () => {
     it('Deve realizar um login com sucesso', () => {
         cy.visit("https://ssfh-aws.souenergy.com.br/login")
         cy.get('#email').type("Nicholas")
-        cy.get('#password').type('123456')
+        cy.get('#password').type('*****')
         cy.get('.btn').click()
 
         //Validação de Login 
