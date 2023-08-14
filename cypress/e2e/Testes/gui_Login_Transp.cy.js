4666bda1f603c14fe5095e507003e65cc604453c
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 16:02:48 -0300

    Update gui_Login_Transp.cy.js

--- cypress/e2e/Testes/gui_Login_Transp.cy.js
+++ cypress/e2e/Testes/gui_Login_Transp.cy.js
@@ -4,7 +4,7 @@
 describe('Teste funcional de Login Transportadora', () => {
     it('Deve realizar um login com sucesso', () => {
         cy.visit("https://souout-homolog.souenergy.com.br/")
-        cy.get('#input-10').type("parthaghosh@gmail24s.xyz")
+        cy.get('#input-10').type("parthaghosh@gmail24s.test")
         cy.get('#input-13').type("*****")
         cy.get('.m-texto').click()   
         //Validação de Login
