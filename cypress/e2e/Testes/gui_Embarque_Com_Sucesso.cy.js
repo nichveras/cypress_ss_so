06bc96662f631e6af33c043cb0c334104f38bb83
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 16:01:54 -0300

    Update gui_Embarque_Com_Sucesso.cy.js

--- cypress/e2e/Testes/gui_Embarque_Com_Sucesso.cy.js
+++ cypress/e2e/Testes/gui_Embarque_Com_Sucesso.cy.js
@@ -4,8 +4,8 @@
 describe('Teste E2E - Realizando a criação de remessa com sucesso', () => {
     it('Fluxo da criação de remessa', () => {
             cy.visit("https://souout-homolog.souenergy.com.br/")
-            cy.get('#input-10').type("Nicholas")
-            cy.get('#input-13').type("*****")
+            cy.get('#input-10').type("test")
+            cy.get('#input-13').type("****")
             cy.get('.m-texto').click()   
             //Validação de Login 
             cy.get('.v-toolbar__title').should('contain','Dashboard')                                                       
