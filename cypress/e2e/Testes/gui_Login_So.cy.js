cd3e33693e8fcb55ba55eae5fb53eab18b0ede48
Nicholas Veras <86290058+nichveras@users.noreply.github.com>
2023-08-14 16:02:27 -0300

    Update gui_Login_So.cy.js

--- cypress/e2e/Testes/gui_Login_So.cy.js
+++ cypress/e2e/Testes/gui_Login_So.cy.js
@@ -4,7 +4,7 @@
 describe('Teste funcional de Login Sou Out', () => {
     it('Deve realizar um login com sucesso', () => {
         cy.visit("https://souout-homolog.souenergy.com.br/")
-        cy.get('#input-10').type("Nicholas")
+        cy.get('#input-10').type("test")
         cy.get('#input-13').type("*****")
         cy.get('.m-texto').click() 
         //Validação de Login 
